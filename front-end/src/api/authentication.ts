76fd7ba690664241c3fcc344ddbdfbf951c55c50
Atomklin <atomklin.innovations@gmail.com>
2023-10-22 00:36:54 -0600

    Add better user response for login

--- front-end/src/api/authentication.ts
+++ front-end/src/api/authentication.ts
@@ -1,9 +1,33 @@
 import axios from "axios";
 
+import { getLanguageCode } from "./utils";
+
 export async function login(username: string, password: string) {
-  return axios.post("/api/auth/login", { username, password });
+  if (!username.length || !password.length)
+    throw Error("Username and Password is required");
+
+  const { status } = await axios.post("/api/auth/login", 
+    { username, password }, 
+    { withCredentials: true });
+
+  if (status !== 200) {
+    throw Error("Invalid username / password");
+
+  } else {
+    const language = getLanguageCode();
+    window.location.pathname = "/" + language + "/dashboard";
+  }
 }
 
 export async function logout() {
-  return axios.post("/api/auth/logout", {}, { withCredentials: true });
+  const { status } = await axios.post("/api/auth/logout", 
+    {}, { withCredentials: true });
+
+  if (status !== 200) {
+    throw Error("A server error occurred while trying to log out");
+
+  } else {
+    const language = getLanguageCode();
+    window.location.pathname = "/" + language + "/login";
+  }
 }
--- front-end/src/components/LoginForm.svelte
+++ front-end/src/components/LoginForm.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { login } from "../api/authentication";
-  import { getLanguageCode } from "../api/utils";
+  import Error from "./Error.svelte";
+  import Loading from "./Loading.svelte";
   import Button from "./forms/Button.svelte";
   import TextInput from "./forms/TextInput.svelte";
 
@@ -10,34 +11,27 @@
 
   let username = "";
   let password = "";
-  let hasError = false;
+  let promise: Promise<void> | undefined;
 
-  /** @todo improve */
-
-  async function onSubmit() {
-    if (!username || !password)
-      hasError = true;
-
-    const { status } = await login(username, password);
-    if (status !== 200)
-      hasError = true;
-    else {
-      const language = getLanguageCode();
-      window.location.pathname = "/" + language + "/dashboard";
-    }
+  function submit() {
+    promise = login(username, password);
   }
 </script>
 
-<form class="w-64 h-fit mx-auto px-11 py-5 gap-2  rounded-lg shadow-md bg-zinc-700  flex flex-col justify-center {hasError ? "ring ring-red-500" : ""}">
+<form class="w-64 h-fit mx-auto px-11 py-5 gap-2  rounded-lg shadow-md bg-zinc-700  flex flex-col justify-center">
   <h1 class="text-3xl font-bold uppercase text-center mb-4">
     {componentTitle}
   </h1>
   <TextInput bind:value={username} id="username" placeholder="Username"/>
   <TextInput bind:value={password} id="password" placeholder="•••••••••" obscure />
-  {#if hasError}
-    <p class="text-red-500 text-sm">{errorMessage}</p>
-  {/if}
+  {#await promise}
+    <Loading />
+  {:catch} 
+    <Error>
+      {errorMessage}
+    </Error>
+  {/await}
   <span class="w-full mt-5">
-    <Button on:click={onSubmit} theme="Default">{buttonTitle}</Button>
+    <Button on:click={submit} theme="Default">{buttonTitle}</Button>
   </span>
 </form>
